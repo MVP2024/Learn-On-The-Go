6c9d97e94bb4a5c1a0737763977c2d8ec680dada
Medvedevy <walery.medvedev@gmail.com>
2025-09-10 13:30:10 +0300

    реализованы допфункции для очистки кеша при создании и сохранении дисциплин, уроков.

--- Lessons/signals.py
+++ Lessons/signals.py
@@ -1,7 +1,9 @@
-from django.db.models.signals import post_save, post_delete
-from django.dispatch import receiver
 from django.core.cache import cache
+from django.db.models.signals import post_delete, post_save
+from django.dispatch import receiver
+
 from .models import Lesson
+
 
 @receiver(post_save, sender=Lesson)
 def clear_lesson_cache_on_save(sender, instance, **kwargs):
@@ -9,6 +11,7 @@ def clear_lesson_cache_on_save(sender, instance, **kwargs):
     Очищает кэш для конкретного урока после его сохранения.
     """
     cache.delete(f"/lessons/{instance.pk}/")
+
 
 @receiver(post_delete, sender=Lesson)
 def clear_lesson_cache_on_delete(sender, instance, **kwargs):
